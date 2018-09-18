cd2c57cb8d50c0c97416c3db78f5585a2ddc6e2e
gahan9 <gahansaraiya@gmail.com>
2018-09-19 00:43:42 +0530

    sync

--- btree_implementation/bPlusTree.py
+++ btree_implementation/bPlusTree.py
@@ -1,6 +1,22 @@
+"""
+some terminology of python:
+_var => (convention only) underscore prefix is just a hint to programmer that a variable or method starting with a single underscore is intended for internal use
+var_ => (convention only) to brake name conflict
+__var => “dunders” (name mangling) rewrite the attribute name in order to avoid naming conflicts in subclasses.
+            interpreter changes the name of the variable in a way that makes it harder to create collisions when the class is extended later.
+"""
+
+import math
+from bisect import bisect_right, bisect_left
+from collections import deque
+
+__author__ = "Gahan Saraiya"
+
+
 class InternalNode(object):
     """
-    Class : B Tree Node
+    Class : B+ Tree Internal Node
+    represents internal (non-leaf) node in B+ tree
     """
     def __init__(self, degree=4):
         """
@@ -26,13 +42,46 @@ class InternalNode(object):
 
     @property
     def is_balanced(self):
+        # return False if total keys exceeds max accommodated keys (degree - 1)
         return self.total_keys <= self.max_data
 
+    def is_empty(self):
+        return self.total_keys <= math.floor(self.degree / 2)
 
-class Btree(object):
+
+class LeafNode(object):
+    """
+    Class : B+ Tree Leaf Node
+    represents leaf node in B+ tree
+    """
     def __init__(self, degree=4):
         self.degree = degree
-        self.root = InternalNode(degree=degree)
+        self.keys = []  # data values
+        self.sibling = None  # sibling node to point
+        self.parent = None  # parent node - None for root node
+
+    @property
+    def is_leaf(self):
+        return True
+
+    @property
+    def total_keys(self):
+        return len(self.keys)
+
+    @property
+    def is_balanced(self):
+        # return False if total keys exceeds max accommodated data (degree - 1)
+        return self.total_keys <= self.degree - 1
+
+    def is_empty(self):
+        return self.total_keys <= math.ceil(self.degree / 2)
+
+
+class BPlusTree(object):
+    def __init__(self, degree=4):
+        self.degree = degree
+        self.__root = LeafNode(degree=degree)
+        self.__leaf = self.__root
 
     def search_node(self, start_node, value):
         """
@@ -89,7 +138,40 @@ class Btree(object):
             sibling2_node.parent = node
             sibling1_node.keys, sibling2_node.keys = node.keys[:center_idx], node.keys[center_idx:]
 
+    def split_leaf(self, node):
+        mid = (self.degree + 1) // 2
+        new_leaf = LeafNode(self.degree)
+        new_leaf.keys = node.keys[mid:]
+        if node.parent is None:  # None and 0 are to be treated as different value
+            parent_node = InternalNode(self.degree)  # create new parent for node
+            parent_node.keys, parent_node.kids = [node.keys[mid].key], [node, new_leaf]
+            node.par = new_leaf.par = parent_node
+            self.__root = parent_node
+        else:
+            i = node.parent.children.index(node)
+            node.parent.keys.insert(i, node.keys[mid].key)
+            node.parent.children.insert(i + 1, new_leaf)
+            new_leaf.parent = node.parent
+        node.keys = node.keys[:mid]
+        node.sibling = new_leaf
+
     def insert(self, value):
+        node = self.__root
+
+        if node.is_leaf:
+            idx = bisect_right(node.keys, value)  # bisect and get index value of where to insert value in node.keys
+            node.keys.insert(idx, value)
+            if not node.is_balanced:
+                self.split_leaf(node)
+            else:
+                return
+        else:
+            if not node.is_balanced:
+                self.insert(self.split(node))
+            else:
+                _index = bisect_right(node.keys, value)
+                self.insert(node.children[_index])
+
         if not self.root.keys:
             # initially no keys/data value - initialize root
             self.root.keys.insert(0, value)
@@ -113,9 +195,9 @@ class Btree(object):
 
 
 if __name__ == "__main__":
-    b = Btree()
+    b = BPlusTree()
     b.insert(5)
     b.insert(1)
     b.insert(15)
-    print(b.root)
-    print(b.root)
+    print(b)
+    print(b)
