5ad097b27feb80a1360f78913dae9a3278b4610d
gahan9 <gahansaraiya@gmail.com>
2018-09-18 17:42:03 +0530

    added document of query

--- btree_implementation/btree.py
+++ btree_implementation/btree.py
@@ -1,48 +1,121 @@
-class BtreeNode(object):
-    def __init__(self, order=4):
-        self.order = order
-        self.keys = []
-        self.children = []
+class InternalNode(object):
+    """
+    Class : B Tree Node
+    """
+    def __init__(self, degree=4):
+        """
+        initialize B tree node
+        :param degree: specify degree of btree  # default degree set to 4
+        """
+        self.degree = degree
+        self.max_data = self.degree-1  # number of keys per node
+        self.keys = []  # store keys/data values
+        self.children = []  # store child nodes (list of instances of BtreeNode); empty list if node is leaf node
         self.parent = None
 
-    @property
-    def enum_child(self):
-        return enumerate(self.children)
-
-    @property
-    def enum_keys(self):
-        return enumerate(self.keys)
-
-    @property
-    def is_root(self):
-        return not self.parent
+    def __repr__(self):
+        return " | ".join(map(str, self.keys))
 
     @property
     def is_leaf(self):
-        return False if self.children else True
+        return False
 
     @property
     def total_keys(self):
-        return len(self.children)
+        return len(self.keys)
 
     @property
     def is_balanced(self):
-        # check if node is balanced or not
-        if self.is_root:
-            return True if self.total_keys < self.order else False
+        return self.total_keys <= self.max_data
+
+
+class Btree(object):
+    def __init__(self, degree=4):
+        self.degree = degree
+        self.root = InternalNode(degree=degree)
+
+    def search_node(self, start_node, value):
+        """
+
+        :param start_node: get root or any non leaf node
+        :param value: value to be search
+        :return: most matching leaf node
+        """
+        if start_node.is_leaf:
+            return start_node
         else:
-            pass
+            for index in range(len(start_node.keys)-1):  # search for next possible node
+                if value < start_node.keys[index]:  # key is greater than value to be search
+                    return self.search_node(start_node.children[0], value)
+                elif start_node.keys[index] <= value < start_node.keys[index+1]:
+                    return self.search_node(start_node.children[index], value)
+                else:
+                    return self.search_node(start_node.children[index+1], value)
 
-    @staticmethod
-    def insert(node):
-        # TODO: implementation
-        if node.is_balanced:
-            pass
+    def search(self, value):
+        """
+        :param value: value to be search
+        :return: leaf node containing value else False
+        tuple of 3 values (status, msg, node)
+        0 - status of success or failure
+        1 - message status
+        2 - last explored node
+        """
+        status, msg, node = None, "", None
+        if not self.root.keys:
+            # initially no keys/data value - initialize root first
+            # msg += "No data in tree..!!"
+            status, msg, node = False, "No data in tree..!!", self.root
+        else:
+            node = self.search_node(self.root, value)
+            if node:
+                # if current node is leaf then check if value exist in current node or not
+                try:
+                    node.keys.index(value)
+                    status, msg, node = True, "Value found in root node", node
+                except ValueError:
+                    status, msg, node = False, "Value not found", self.root
+        return status, msg, node
 
-    def search(self):
-        # TODO: searching
-        pass
+    def split(self, node):
+        center_idx = len(node.keys) // 1  # center node index
+        median = node.keys[center_idx - 1]  # key to be shift to upper level
+        sibling1_node, sibling2_node = BtreeNode(), BtreeNode()
+        if not node.parent:
+            # split root node for overflow
+            node.keys = [median]
+            node.children = [sibling1_node, sibling2_node]
+            sibling1_node.parent = node
+            sibling2_node.parent = node
+            sibling1_node.keys, sibling2_node.keys = node.keys[:center_idx], node.keys[center_idx:]
+
+    def insert(self, value):
+        if not self.root.keys:
+            # initially no keys/data value - initialize root
+            self.root.keys.insert(0, value)
+            status, msg, node = True, "LOL!! it was first value!!", self.root
+        else:
+            status, msg, node = self.search(value)
+            for index in range(len(node.keys)):
+                if value < node.keys[index]:  # key is greater than value to be search
+                    node.keys.insert(index, value)
+                elif node.keys[index] <= value < node.keys[index + 1]:
+                    node.keys.insert(index, value)
+                else:
+                    node.keys.insert(index+1, value)
+            if node.is_balanced:
+                status, msg = True, "Balanced - No Split Require"
+            else:
+                msg = "splitting require"
+                self.split(node)
+        print(msg, node.keys)
+        return status, msg, node
 
 
 if __name__ == "__main__":
-    b = BtreeNode()
+    b = Btree()
+    b.insert(5)
+    b.insert(1)
+    b.insert(15)
+    print(b.root)
+    print(b.root)
--- practical_1.md
+++ practical_1.md
@@ -1,4 +1,95 @@
-select * from ds_leaderboard;
+Practical 1
+===========
+> a) Import Database, understand Query execution plan using SQL Yog  
+> b) Analyze impact of index , type of index on query performance
 
-![select_all]()
+a) Database Structure
+---------------------
+- Database: **MySQL**  
+- Total data rows in table ds_leaderboard: **4,00,000**
+- PC Configuration  
+    : Intel® Core™ i3-4130 CPU @ 3.40GHz × 4  
+    : ~4 GB RAM
+
+**ds_leaderboard**
+
+    CREATE TABLE `ds_leaderboard` ( 
+        `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+        `name` VarChar( 64 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        `email` VarChar( 254 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        `organizer` VarChar( 256 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        `score` Double( 22, 0 ) NOT NULL,
+        `time` VarChar( 16 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        `country` VarChar( 128 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        `language_id` Int( 11 ) NULL,
+        PRIMARY KEY ( `id` ) )
+    CHARACTER SET = utf8
+    COLLATE = utf8_general_ci
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 400001;
+-- -------------------------------------------------------------
+**ds_programminglanguage**
+
+    CREATE TABLE `ds_programminglanguage` ( 
+        `id` Int( 11 ) AUTO_INCREMENT NOT NULL,
+        `name` VarChar( 32 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+        `version` VarChar( 16 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+        `release` Date NULL,
+        PRIMARY KEY ( `id` ) )
+    CHARACTER SET = utf8
+    COLLATE = utf8_general_ci
+    ENGINE = InnoDB
+    AUTO_INCREMENT = 13;
+-- -------------------------------------------------------------
+
+b) Impact of index
+------------------
+### Query
+
+    select * from ds_leaderboard;
+Query time: 1.947 s  
+Index used : No index to be used because we are loading all data of all attribute(column)
+
+### Query
+
+    select * from ds_leaderboard where language_id=1;
+Query time: 235 ms  
+No. of records: 33243  
+Index used : index on attribute language_id  
+
+### Query
+
+    select * from ds_leaderboard ignore index(pl) where language_id=1;
+Query time: 287 ms  
+No. of records: 33243  
+Index used : (No index) ignoring index on attribute language_id  
+
+### Query
+
+    select name,language_id from ds_leaderboard where language_id between 1 and 5;
+Query time: 226 ms  
+No. of records: 166490  
+Index used : (No index used) (possible key on language_id)  
+
+
+### Query
+
+    select name,language_id from ds_leaderboard use index(pl) where language_id between 1 and 5;
+
+|  |  |
+|---|---|
+| Query time | 206 ms |  
+| No. of records | 166490 |  
+| Index used | forced to use index on language_id |  
+
+
+### Query
+
+    select name,language_id from ds_leaderboard use index(pl) where language_id between 1 and 5;
+
+|  |  |
+|---|---|
+| Query time | 206 ms |  
+| No. of records | 166490 |  
+| Index used | forced to use index on language_id |  
 
