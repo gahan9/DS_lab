9555e9210bea9376b7ce9073e4aa5627fce5edeb
gahan9 <gahansaraiya@gmail.com>
2018-09-25 13:20:26 +0530

    sync

---
+++ practical_4.md
@@ -0,0 +1,10 @@
+[Practical 3](https://github.com/gahan9/DS_lab/blob/master/practical_3.md)
+===
+> Implementation of Extendible Hashing
+
+
+
+Implementation
+------
+```python
+```
---
+++ practical_4/external_hashing.py
@@ -0,0 +1,11 @@
+class Bucket(object):
+    def __init__(self, bucket_size=2):
+        pass
+
+
+class GlobalBucket(object):
+    pass
+
+
+if __name__ == "__main__":
+    g = GlobalBucket()
