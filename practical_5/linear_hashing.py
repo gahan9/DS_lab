c3cd836e33140c754993f06b0a0da979e9c3c62f
gahan9 <gahansaraiya@gmail.com>
2018-10-10 02:31:29 +0530

    fixed bug in linear hashing

--- practical_5/linear_hashing.py
+++ practical_5/linear_hashing.py
@@ -9,6 +9,7 @@ Implementation of linear hashing
 """
 from math import log2
 
+
 class LinearHashing(object):
     def __init__(self, *args, **kwargs):
         self.threshold = kwargs.get('threshold', 0.7)
@@ -33,35 +34,40 @@ class LinearHashing(object):
     def hash_function(self, value, flag=0):
         if not flag:
             # if no splitting require
-            return value % (2**self.current_phase)
+            return value % (2 ** self.current_phase)
         else:
             # if splitting require
-            return value % (2**(self.current_phase + 1))
+            return value % (2 ** (self.current_phase + 1))
 
     def set_index_counter_if(self):
         # set index counter from where splitting to be done to 0 when phase changes
         if self.current_phase != self.previous_phase:
             self.index_counter = 0
 
-    def insert(self, value):
+    def insert(self, value, print_status=0):
         self.set_index_counter_if()
 
         if self.threshold_outbound:
             # buffer to be extend
+            self.buffer[len(self.buffer)] = []
             buffer_index = self.hash_function(value, flag=1)
             self.buffer[buffer_index] = self.buffer.setdefault(buffer_index, []) + [value]
             # bucket to be split
             bucket_to_split = self.buffer[self.index_counter]
             self.buffer[self.index_counter] = []
             for data in bucket_to_split:
-                buffer_index = self.hash_function(data)
-                self.buffer[buffer_index] = self.buffer.setdefault(buffer_index, []) + [data]
+                buffer_idx = self.hash_function(data, flag=1)
+                self.buffer[buffer_idx] = self.buffer.setdefault(buffer_idx, []) + [data]
             self.index_counter += 1
         else:
             buffer_index = self.hash_function(value)
             # self.buffer[buffer_index].append(value)
             self.buffer[buffer_index] = self.buffer.setdefault(buffer_index, []) + [value]
         self.total_data += 1
+
+        if print_status:
+            print(self.buffer)
+            print("INDEX: {}\nCURRENT PHASE: {} \t PREVIOUS PHASE: {}".format(self.index_counter, self.current_phase, self.previous_phase))
 
     def delete(self):
         return NotImplementedError
@@ -72,14 +78,13 @@ class LinearHashing(object):
     def __str__(self):
         return "\n".join("{} -> {}".format(k, v) for k, v in self.buffer.items())
 
+
 if __name__ == "__main__":
     l = LinearHashing()
     input_lis = [3, 2, 4, 1, 8, 14, 5, 10, 7, 24, 17, 13, 15]
     for i in input_lis:
-        print("inserting: ", i)
-        print("STATUS:-------")
-        print("INDEX: {}\nCURRENT PHASE: {} \t PREVIOUS PHASE: {}".format(l.index_counter, l.current_phase, l.previous_phase))
-        print("-"*40)
-        print(">>>>> ", l.buffer)
-        l.insert(i)
+        # print("STATUS:-------")
+        # print(">>>>> ", l.buffer)
+        print("{1} Inserting: {0} {1}".format(i, "#"*40))
+        l.insert(i, print_status=0)
     print(l)
